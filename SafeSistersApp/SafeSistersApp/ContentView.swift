e028561761d6331427ede8be99bfd54915adc7ad
Sofía A01571120 <88906709+sofia-cantu@users.noreply.github.com>
2024-10-21 21:46:35 -0600

    Prueba 2 commit

--- SafeSistersApp/SafeSistersApp/ContentView.swift
+++ SafeSistersApp/SafeSistersApp/ContentView.swift
@@ -24,7 +24,7 @@ struct ContentView: View {
                     .foregroundColor(.white)
                     .padding(.top, 5)
                 
-                Text("Prueba")
+                Text("Prueba2")
                 
                 Spacer()
                 
