ca47391cf0b3fe187b7120f9d123ad16f66a9ff2
Sofía A01571120 <88906709+sofia-cantu@users.noreply.github.com>
2024-10-22 22:47:05 -0600

    Fx: RecursosView y PreventionView

---
+++ SafeSistersApp/SafeSistersApp/Models/PreventionModel.swift
@@ -0,0 +1,8 @@
+//
+//  PreventionModel.swift
+//  SafeSistersApp
+//
+//  Created by Sofía Cantú on 22/10/24.
+//
+
+import Foundation
---
+++ SafeSistersApp/SafeSistersApp/ViewModels/PreventionViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PreventionViewModel.swift
+//  SafeSistersApp
+//
+//  Created by Sofía Cantú on 22/10/24.
+//
+
+import Foundation
